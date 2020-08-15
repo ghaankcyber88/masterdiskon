c7120d96d1cd77b331cf74fa0f918713d7a44f07
matadesain <matadesaindotcom@gmail.com>
2020-08-15 13:40:45 +0700

    asd

--- app/screens/SignIn/index.js
+++ app/screens/SignIn/index.js
@@ -61,7 +61,7 @@ class SignIn extends ValidationComponent {
 
         GoogleSignin.configure({
           scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-          webClientId: '254255805430-c16v2nifqsa42a8iduiv552uihrjbl7u.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+          webClientId: '280725445152-ulhq6j2enufgiedabbph68i2tg6clilm.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
           offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
           hostedDomain: '', // specifies a hosted domain restriction
           loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
